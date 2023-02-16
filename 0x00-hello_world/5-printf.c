351847b0da65a086da407803358066df53ac92d6
Ahmed Mohamed Lotfi <moahmedm559@gmail.com>
2023-02-16 11:40:11 -0800

    print a sentence using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: This main function print this sentence and a new line after it using printf function.
  * Return: Always 0 (Success)
  */
 int main(void)
