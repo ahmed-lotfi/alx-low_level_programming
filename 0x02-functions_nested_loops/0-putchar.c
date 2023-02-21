42f9ce7152eefa068b2f1738eb79b1422b88b626
Ahmed Mohamed Lotfi <moahmedm559@gmail.com>
2023-02-21 10:01:06 -0800

    print the word -putchar followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main: Entry point
+ * return: 0 always
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+Task 0. _putchar
+program that prints _putchar, followed by a new line.
+
+The program should return 0
